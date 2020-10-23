ae817444f28db033f3b709cab7ab490798a55803
anthony azzi <anthony.mi.elazzi@net.usek.edu.lb>
2020-10-23 11:33:41 +0300

    commit

--- app/src/main/java/a/e/trygit/MainActivity.java
+++ app/src/main/java/a/e/trygit/MainActivity.java
@@ -3,14 +3,18 @@ package a.e.trygit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 TextView T1;
+Button B1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         T1= findViewById(R.id.sa);
+        B1= findViewById(R.id.button);
+
     }
 }
